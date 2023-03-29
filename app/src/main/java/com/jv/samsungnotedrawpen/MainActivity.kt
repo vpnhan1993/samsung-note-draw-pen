a262420d90f540b0492622b3b0cbe94eb27887b3
jv <vpnhan1993@gmail.com>
2023-03-29 10:10:03 +0700

    eraser

--- app/src/main/java/com/jv/samsungnotedrawpen/MainActivity.kt
+++ app/src/main/java/com/jv/samsungnotedrawpen/MainActivity.kt
@@ -10,7 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val board = findViewById<DrawingBoard>(R.id.board)
-        findViewById<Button>(R.id.eraser).setOnClickListener { board.setEraser() }
     }
 }
